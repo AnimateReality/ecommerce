4b5df7143161769b982d219448b51d84fe911d45
AnimateReality <ronaldchittinappilly@gmail.com>
2022-07-09 13:11:50 +1200

    Product Update
    Uploaded first product

--- pages/index.js
+++ pages/index.js
@@ -9,10 +9,10 @@ const Home = ({ products, bannerData }) => {
     {console.log(bannerData)}
     <div className='products-heading'>
       <h2>Best Selling Products</h2>
-      <p>All the speakers you can think of. Test. Changed env test</p>
+      <p>Select from a wide range of quality speakers and headphones.</p>
     </div>
     <div className='products-container'> 
-      {products?.map((product) => (product.name)
+      {products?.map((product) => <Product key={product._id} product={product}/>
       )}
     </div>
     Footer
