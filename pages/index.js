9dc3bc0f8a4b3cafc92eb210dc0f2efe11898e60
AnimateReality <ronaldchittinappilly@gmail.com>
2022-07-11 23:55:25 +1200

    New Product
    Added new product through sanity and started linking footer banner

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ const Home = ({ products, bannerData }) => {
       {products?.map((product) => <Product key={product._id} product={product}/>
       )}
     </div>
-    Footer
+    <FooterBanner footerBanner={bannerData && bannerData[0]}/>
     </>
   )
 }
